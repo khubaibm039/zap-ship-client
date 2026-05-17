c3e90251b7f50cf0e0892f4a0f33314390ed86b8
khubaibm039 <khubaibm039@gmail.com>
2026-05-18 02:20:47 +0600

    react hook form

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,8 +1,64 @@
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const handleRegister = (data) => {
+        console.log("after register", data);
+    };
+
     return (
         <div>
-            <h1>this is the register page</h1>
+            <form action="" onSubmit={handleSubmit(handleRegister)}>
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input
+                        type="email"
+                        {...register("email", { required: true })}
+                        className="input"
+                        placeholder="Email"
+                    />
+                    {errors.email?.type === "required" && (
+                        <p className="text-red-500 mt-1" role="alert">
+                            Email is required
+                        </p>
+                    )}
+                    <label className="label">Password</label>
+                    <input
+                        type="password"
+                        {...register("password", {
+                            required: true,
+                            minLength: 6,
+                            pattern:
+                                /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+                        })}
+                        className="input"
+                        placeholder="Password"
+                    />
+                    {errors.password?.type === "required" && (
+                        <p className="text-red-500 mt-1" role="alert">
+                            Password is required
+                        </p>
+                    )}
+                    {errors.password?.type === "minLength" && (
+                        <p className="text-red-500 mt-1" role="alert">
+                            Password must be at least 6 characters
+                        </p>
+                    )}
+                    {errors.password?.type === "pattern" && (
+                        <p className="text-red-500 mt-1" role="alert">
+                            Password must contain at least one uppercase letter, one digit, and one special character
+                        </p>
+                    )}
+                    <div>
+                        <a className="link link-hover">Forgot password?</a>
+                    </div>
+                    <button className="btn btn-neutral mt-4">Register</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
